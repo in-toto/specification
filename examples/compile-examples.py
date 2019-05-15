088277b841f6f94b0b6397b7baceb701fe4c43a4
Lukas Puehringer <lukas.puehringer@nyu.edu>
2019-05-15 11:18:26 +0200

    Add beautify function to example compiler
    Add function that recursively traverses a passed python object,
    e.g. in-toto metadata, allowing to truncate long strings, lists and
    dicts, and to reorder dict keys, using OrderedDict.

--- examples/compile-examples.py
+++ examples/compile-examples.py
@@ -24,7 +24,8 @@ import json
 import argparse
 import glob
 import random
-
+import six
+from collections import OrderedDict
 from pprint import pformat
 
 MAIN_TEMPLATE = """{name}
@@ -49,6 +50,85 @@ LINK_TEMPLATE = """
 ```
 
 """
+
+def beautify(obj, max_str_len=70, max_list_len=10, max_dict_len=10,
+    dict_key_order_lists=None):
+  """Beautify the passed object, truncating long strings, lists
+  and dictionaries, and printing dictionary keys in the order passed by
+  `dict_key_orders`.
+
+  Truncation:
+    - the last 3 characters of a truncated string are replaced with ellipsis
+    - the last item of a truncated list is replaced with ellipsis
+    - a dict is transformed to an OrderedDict and the last key and value are
+      each replaced with ellipsis
+
+  Dict ordering:
+    Dict keys on any level of the passed obj are compared with all lists in
+    the passed `dict_key_order_lists`. If there is a match the passed order
+    is used. Note that the keys of a truncated dict without the ellipsis
+    key is used for comparison.
+
+  """
+  if max_str_len < 3:
+    raise ValueError("'max_str_len' must be >= 3, got: " + str(max_str_len))
+
+  if max_list_len < 1:
+    raise ValueError("'max_list_len' must be >= 1, got: " + str(max_list_len))
+
+  if max_dict_len < 1:
+    raise ValueError("'max_dict_len' must be >= 1, got: " + str(max_dict_len))
+
+  return _beautify(obj, max_str_len=max_str_len, max_list_len=max_list_len,
+      max_dict_len=max_dict_len, dict_key_order_lists=dict_key_order_lists)
+
+
+def _beautify(obj, **kw):
+  """Recurisve helper for beautify. See beautify for more info. """
+
+  # Truncate strings
+  if isinstance(obj, six.string_types):
+    if len(obj) > kw["max_str_len"]:
+      obj = obj[:kw["max_str_len"] - 3] + "..."
+
+  # Truncate list and recurse into _beautify for each  item
+  elif isinstance(obj, list):
+    if len(obj) > kw["max_list_len"]:
+      obj = obj[:kw["max_list_len"] - 1] + ["..."]
+
+    tmp_obj = []
+    for item in obj:
+      tmp_obj.append(_beautify(item, **kw))
+    obj = tmp_obj
+
+  # Truncate dict and recurse into _beautify for each item
+  elif isinstance(obj, dict):
+    dict_keys = list(obj.keys())
+
+    # If the keys of this dict is the same set as any of the passed
+    # ordered key lists, we use that order below
+    for key_order in kw["dict_key_order_lists"]:
+      if set(dict_keys) == set(key_order):
+        dict_keys = key_order
+        break
+
+    truncate = len(dict_keys) > kw["max_dict_len"]
+    if truncate:
+      dict_keys = dict_keys[:kw["max_dict_len"] - 1]
+
+    # An ordered dict is required for both, ordering the dict keys and also
+    # to add a terminal ellipsis pair in case of truncation
+    tmp_obj = OrderedDict()
+    for key in dict_keys:
+      tmp_obj[key] = _beautify(obj[key], **kw)
+
+    if truncate:
+      tmp_obj["..."] = "..."
+
+    obj = tmp_obj
+
+  return obj
+
 
 def load_layout_metadata(folder):
   return _load_metadata(folder, 'root.layout')
